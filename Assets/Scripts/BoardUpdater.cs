ca5538af896180ddb854be9f5f54786d4ec56e85
IQ <iq.at.sk131@gmail.com>
2016-12-05 11:39:30 +0700

    Add Brush & Fix For Android Build

--- Assets/Scripts/BoardUpdater.cs
+++ Assets/Scripts/BoardUpdater.cs
@@ -12,15 +12,24 @@ using UnityEngine.UI;
 // necessary setup (initializing the firebase app, etc) on
 // startup.
 public class BoardUpdater : MonoBehaviour {
-	
+
 	private const string DatabaseUrl = "https://virtualboard-5b7ad.firebaseio.com/";
 	private DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+	public static BoardUpdater Instance;
+	public const int Width = 80;
+	public const int Height = 60;
+	public DatabaseReference Reference;
+	public bool[,] Board;
 
 	// When the app starts, check to make sure that we have
 	// the required dependencies to use Firebase, and if not,
 	// add them if possible.
 	void Start() {
-		Debug.Log("Start");
+		Instance = this;
+		Board = new bool[Width,Height];
+
+		Debug.Log("Start!!");
+
 		dependencyStatus = FirebaseApp.CheckDependencies();
 		if (dependencyStatus != DependencyStatus.Available) {
 			FirebaseApp.FixDependenciesAsync().ContinueWith(task => {
@@ -40,16 +49,20 @@ public class BoardUpdater : MonoBehaviour {
 	}
 
 	// Initialize the Firebase database:
-	void InitializeFirebase() {
-		Debug.Log("Initial Firebase");
+	private void InitializeFirebase() {
+
+		Debug.Log("Initial Firebase!!");
+
 		// Set up the Editor before calling into the realtime database.
 		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(DatabaseUrl);
-		FirebaseApp.DefaultInstance.SetEditorAuthUserId("q3uFGcyaB3OHJ5wuTdAyMvBsaQ02");
+		//FirebaseApp.DefaultInstance.SetEditorAuthUserId("q3uFGcyaB3OHJ5wuTdAyMvBsaQ02");
 
 		// Get the root reference location of the database.
-		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+		Reference = FirebaseDatabase.DefaultInstance.RootReference;
 
-		reference.ChildAdded += (object sender, ChildChangedEventArgs args) => {
+		Debug.Log("Add Event Listener!!");
+
+		Reference.ChildAdded += (object sender, ChildChangedEventArgs args) => {
 			if (args.DatabaseError != null) {
 				Debug.LogError(args.DatabaseError.Message);
 				return;
@@ -59,11 +72,12 @@ public class BoardUpdater : MonoBehaviour {
 			Debug.Log("Added: " + x + ", " + y);
 			GameObject pixel = GameObject.Instantiate(Resources.Load("Pixel")) as GameObject;
 			pixel.transform.position = new Vector3(x,0,y);
+			Board[x,y]=true;
 			if(GameObject.Find("p" + x + "," + y) == null){
 				pixel.name="p" + x + "," + y;
 			}
 		};
-		reference.ChildRemoved += (object sender, ChildChangedEventArgs args) => {
+		Reference.ChildRemoved += (object sender, ChildChangedEventArgs args) => {
 			if (args.DatabaseError != null) {
 				Debug.LogError(args.DatabaseError.Message);
 				return;
@@ -71,9 +85,29 @@ public class BoardUpdater : MonoBehaviour {
 			int x=int.Parse(args.Snapshot.Key.Substring(1,4));
 			int y=int.Parse(args.Snapshot.Key.Substring(6,4));
 			Debug.Log("Removed: " + x + ", " + y);
+			Board[x,y]=false;
 			if(GameObject.Find("p" + x + "," + y) != null){
 				GameObject.Destroy(GameObject.Find("p" + x + "," + y));
 			}
 		};
+	}
+
+	public void WriteBoard(double x, double y){
+		WriteBoard((int)x, (int)y);
+	}
+
+	public void WriteBoard(int x, int y){
+		if(x < 0 || x > Width || y < 0 || y > Height) return ;
+		if(Board[x,y] == true) return ;
+		if(Reference != null){
+			string key = "p" + x.ToString().PadLeft(4,'0') + "," + y.ToString().PadLeft(4,'0');
+			Reference.Child(key).SetValueAsync("true");
+			GameObject pixel = GameObject.Instantiate(Resources.Load("Pixel")) as GameObject;
+			pixel.transform.position = new Vector3(x,0,y);
+			Board[x,y]=true;
+			if(GameObject.Find("p" + x + "," + y) == null){
+				pixel.name="p" + x + "," + y;
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Brush.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brush : MonoBehaviour {
+
+	//[SerializeField] GameObject WhiteBoard;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.x < 0 || this.transform.position.x > BoardUpdater.Width || this.transform.position.z < 0 || this.transform.position.z > BoardUpdater.Height) return ;
+		if(this.transform.position.y <1 && BoardUpdater.Instance.Board[(int)this.transform.position.x, (int)this.transform.position.z] == false){
+			Debug.Log("Draw at " + this.transform.position.x.ToString().PadLeft(4, '0') + "," + this.transform.position.z.ToString().PadLeft(4, '0'));
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x, this.transform.position.z);
+			/*BoardUpdater.Instance.WriteBoard(this.transform.position.x-2, this.transform.position.z);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x+2, this.transform.position.z);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x, this.transform.position.z-2);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x, this.transform.position.z+2);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x-1, this.transform.position.z);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x+1, this.transform.position.z);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x, this.transform.position.z-1);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x, this.transform.position.z+1);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x+1, this.transform.position.z+1);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x+1, this.transform.position.z-1);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x-1, this.transform.position.z+1);
+			BoardUpdater.Instance.WriteBoard(this.transform.position.x-1, this.transform.position.z-1);*/
+		}
+	}
+}
